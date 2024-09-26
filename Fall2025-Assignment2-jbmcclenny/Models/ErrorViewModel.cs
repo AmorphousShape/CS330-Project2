10455a02057e33f10433703557c7f336d113cae0
Jace <jacemcclenny@gmail.com>
2024-09-26 13:18:06 -0500

    initial commit

---
+++ Fall2025-Assignment2-jbmcclenny/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Assignment2_jbmcclenny.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
